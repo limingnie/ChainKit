43d22d16fc6e8c46f76ccd7264922d57d518235d
Avario <avario@roamltd.com>
2017-02-15 10:22:16 +1300

    Carthage Build

---
+++ ChainKit/Carthage/Build/iOS/ChainKit.framework/Headers/ChainKit.h
@@ -0,0 +1,18 @@
+//
+//  ChainKit.h
+//  ChainKit
+//
+//  Created by Avario.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChainKit.
+FOUNDATION_EXPORT double ChainKitVersionNumber;
+
+//! Project version string for ChainKit.
+FOUNDATION_EXPORT const unsigned char ChainKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChainKit/PublicHeader.h>
+
+
